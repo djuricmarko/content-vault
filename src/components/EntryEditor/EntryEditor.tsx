400c84dca66365fc1533ae70024d37a801e49934
Marko Djuric <markodjuric94@proton.me>
2025-12-11 22:57:05 +0100

    fix: editor toolbar

--- src/components/EntryEditor/EntryEditor.tsx
+++ src/components/EntryEditor/EntryEditor.tsx
@@ -1,26 +1,47 @@
 'use client';
 
 import { useEditor, EditorContent } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
+import { BulletList, ListItem, OrderedList } from '@tiptap/extension-list';
+import { Color, TextStyle } from '@tiptap/extension-text-style';
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from "@tiptap/extension-text";
+import Heading from "@tiptap/extension-heading";
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
+import Strike from "@tiptap/extension-strike";
+import Code from "@tiptap/extension-code";
 import { Toolbar } from "./Toolbar";
 import styles from './entry-editor.module.css';
 
 export function EntryEditor() {
   const editor = useEditor({
     extensions: [
-      StarterKit.configure({
-        heading: {
-          levels: [1, 2, 3],
-        },
+      Document,
+      Paragraph,
+      Text,
+      Heading.configure({
+        levels: [1, 2, 3],
       }),
+      Bold,
+      Italic,
+      Strike,
+      Code,
+      BulletList,
+      OrderedList,
+      ListItem,
+      Color,
+      TextStyle
     ],
-    content: '<p>Start writing your entry...</p>',
     autofocus: true,
     immediatelyRender: false,
     editorProps: {
       attributes: {
         class: styles.content,
       },
+    },
+    onUpdate: ({ editor }) => {
+      console.log(editor.getHTML());
     },
   });
 
--- src/components/EntryEditor/Toolbar.tsx
+++ src/components/EntryEditor/Toolbar.tsx
@@ -1,5 +1,6 @@
+import { Bold, Code, Heading1, Heading2, Heading3, Italic, List, ListOrdered, Strikethrough } from "lucide-react";
 import type { Editor } from "@tiptap/react";
-import styles from "@/components/EntryEditor/entry-editor.module.css";
+import styles from "./entry-editor.module.css";
 
 function getButtonClass(isActive: boolean) {
   return isActive ? `${styles.button} ${styles.isActive}` : styles.button;
@@ -15,13 +16,19 @@ export function Toolbar({ editor }: { editor: Editor | null }) {
           onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
           className={getButtonClass(editor.isActive('heading', { level: 1 }))}
         >
-          H1
+          <Heading1 />
         </button>
         <button
           onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
           className={getButtonClass(editor.isActive('heading', { level: 2 }))}
         >
-          H2
+          <Heading2 />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={getButtonClass(editor.isActive('heading', { level: 3 }))}
+        >
+          <Heading3 />
         </button>
       </div>
 
@@ -32,25 +39,25 @@ export function Toolbar({ editor }: { editor: Editor | null }) {
           onClick={() => editor.chain().focus().toggleBold().run()}
           className={getButtonClass(editor.isActive('bold'))}
         >
-          B
+          <Bold />
         </button>
         <button
           onClick={() => editor.chain().focus().toggleItalic().run()}
           className={getButtonClass(editor.isActive('italic'))}
         >
-          I
+          <Italic />
         </button>
         <button
           onClick={() => editor.chain().focus().toggleStrike().run()}
           className={getButtonClass(editor.isActive('strike'))}
         >
-          S
+          <Strikethrough />
         </button>
         <button
           onClick={() => editor.chain().focus().toggleCode().run()}
           className={getButtonClass(editor.isActive('code'))}
         >
-          Code
+          <Code />
         </button>
       </div>
 
@@ -61,44 +68,13 @@ export function Toolbar({ editor }: { editor: Editor | null }) {
           onClick={() => editor.chain().focus().toggleBulletList().run()}
           className={getButtonClass(editor.isActive('bulletList'))}
         >
-          UL
+          <List />
         </button>
         <button
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
           className={getButtonClass(editor.isActive('orderedList'))}
         >
-          OL
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={getButtonClass(editor.isActive('blockquote'))}
-        >
-          &#34;&#34;
-        </button>
-      </div>
-
-      <div className={styles.divider} />
-
-      <div className={styles.buttonGroup}>
-        <button
-          className={styles.button}
-          onClick={() => editor.chain().focus().setHorizontalRule().run()}
-        >
-          Line
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().undo()}
-        >
-          Undo
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().redo()}
-        >
-          Redo
+          <ListOrdered />
         </button>
       </div>
     </div>
