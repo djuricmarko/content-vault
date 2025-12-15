ae37ae1b817f22e5f3302df2136bb12a94869b08
Marko Djuric <markodjuric94@proton.me>
2025-12-16 00:38:07 +0100

    fix: upgrade react and react-dom and remove manual suspense

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, Suspense } from "react";
 import { Sidebar, SidebarSkeleton } from "@/components/sidebar";
-import Loading from "./loading";
 import styles from "./page.module.css";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
@@ -10,9 +9,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         <Sidebar />
       </Suspense>
       <main className={styles.main}>
-        <Suspense fallback={<Loading />}>
-          {children}
-        </Suspense>
+        {children}
       </main>
     </div>
   );
