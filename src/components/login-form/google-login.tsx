e4351df7f206ad8d33697418fee057bdeabc0306
Marko Djuric <markodjuric94@proton.me>
2025-12-16 15:09:27 +0100

    feat: correct google error handling and icon

--- src/components/login-form/google-login.tsx
+++ src/components/login-form/google-login.tsx
@@ -1,42 +1,62 @@
 'use client';
 
 import Image from "next/image";
+import { useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 import styles from './login-form.module.css';
 
 export function GoogleLogin() {
+  const supabase = createClient();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
   async function handleLogin() {
-    const supabase = createClient();
+    setIsLoading(true);
+    setErrorMessage('');
 
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: `${location.origin}/auth/callback`,
-        queryParams: {
-          access_type: 'offline',
-          prompt: 'consent',
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+          redirectTo: `${location.origin}/auth/callback`,
+          queryParams: {
+            access_type: 'offline',
+            prompt: 'consent',
+          },
         },
-      },
-    });
+      });
 
-    if (error) {
-      console.error('Error logging in:', error.message);
+      if (error) {
+        setErrorMessage(error.message);
+      }
+    } catch (error: unknown) {
+      setErrorMessage(error instanceof Error ? error.message : 'An unexpected error occurred.');
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
-    <button
-      type="button"
-      onClick={handleLogin}
-      className={styles.googleBtn}
-    >
-      <Image
-        src="https://authjs.dev/img/providers/google.svg"
-        alt="Google logo"
-        width={20}
-        height={20}
-      />
-      Continue with Google
-    </button>
+    <>
+      <button
+        type="button"
+        onClick={handleLogin}
+        className={styles.googleBtn}
+        disabled={isLoading}
+      >
+        <Image
+          src="/google.svg"
+          alt="Google logo"
+          width={20}
+          height={20}
+        />
+        Continue with Google
+      </button>
+      {errorMessage && (
+        <div className={styles.error}>
+          <p>{errorMessage}</p>
+        </div>
+      )}
+    </>
   );
 }
