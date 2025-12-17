c51abacb925c6380b4f7b0740af789d1ef235f63
Marko Djuric <markodjuric94@proton.me>
2025-12-17 10:33:44 +0100

    fix: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './src/db/migrations',
+  schema: './src/lib/drizzle/schema.ts',
+  out: './src/lib/drizzle/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL_MIGRATION!,
