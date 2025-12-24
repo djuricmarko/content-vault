fc5ad787e227fe3ebbd4821f008b27e790191de4
Marko Djuric <markodjuric94@proton.me>
2025-12-24 21:11:33 +0100

    feat: add delete option

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,26 @@
+import { Menu } from '@base-ui/react/menu';
+import { Ellipsis, Trash } from "lucide-react";
 import styles from "./header.module.css";
 
 export function Header({ title }: { title: string }) {
   return (
     <div className={styles.title}>
       <h2>{title}</h2>
+      <Menu.Root>
+        <Menu.Trigger className={styles.options}>
+          <Ellipsis />
+        </Menu.Trigger>
+        <Menu.Portal>
+          <Menu.Positioner sideOffset={10}>
+            <Menu.Popup className={styles.dropdownContent}>
+              <Menu.Item className={styles.dropdownItem}>
+                <Trash size={16} color="red" />
+                Delete
+              </Menu.Item>
+            </Menu.Popup>
+          </Menu.Positioner>
+        </Menu.Portal>
+      </Menu.Root>
     </div>
   );
 }
